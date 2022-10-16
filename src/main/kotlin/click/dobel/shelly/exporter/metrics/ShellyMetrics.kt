1e9b3d61c708525658dc56805521959264f57c21
Markus Dobel <github@spam.dobel.click>
2022-10-16 17:07:24 +0200

    feat: add meter descriptions.

--- src/main/kotlin/click/dobel/shelly/exporter/metrics/ShellyMetrics.kt
+++ src/main/kotlin/click/dobel/shelly/exporter/metrics/ShellyMetrics.kt
@@ -57,25 +57,100 @@ class ShellyMetrics(
     with(device) {
       val tags = deviceTags(device)
 
-      gauge("power.max", "watts", tags) { settings(address)?.maxPower }
-      boolGauge("cloud.enabled", tags) { settings(address)?.cloud?.enabled }
-      boolGauge("cloud.connected", tags) { settings(address)?.cloud?.connected }
+      gauge(
+        "power.max",
+        "Maximum allowed power consumption before the device switches off.",
+        "watts",
+        tags
+      ) { settings(address)?.maxPower }
+      boolGauge(
+        "cloud.enabled",
+        "Whether Shelly cloud is enabled.",
+        tags
+      ) { settings(address)?.cloud?.enabled }
+      boolGauge(
+        "cloud.connected",
+        "Whether Shelly cloud is connected.",
+        tags
+      ) { settings(address)?.cloud?.connected }
 
-      gauge("location.latitude", "degrees", tags) { settings(address)?.lat }
-      gauge("location.longitude", "degrees", tags) { settings(address)?.lng }
+      gauge(
+        "location.latitude",
+        "Latitude of the configured device location, in degrees.",
+        "degrees",
+        tags
+      ) { settings(address)?.lat }
+      gauge(
+        "location.longitude",
+        "Longitude of the configured device location, in degrees.",
+        "degrees",
+        tags
+      ) { settings(address)?.lng }
 
-      gauge("temperature", "degrees.celsius", tags) { status(address)?.temperature?.celsius }
-      gauge("temperature", "degrees.fahrenheit", tags) { status(address)?.temperature?.fahrenheit }
-      boolGauge("temperature.valid", tags) { status(address)?.temperature?.isValid }
-      boolGauge("temperature.overheated", tags) { status(address)?.overTemperature }
+      gauge(
+        "temperature",
+        "Device temperature in degrees celsius.",
+        "degrees.celsius",
+        tags
+      ) { status(address)?.temperature?.celsius }
+      gauge(
+        "temperature",
+        "",
+        "degrees.fahrenheit",
+        tags
+      ) { status(address)?.temperature?.fahrenheit }
+      boolGauge(
+        "temperature.valid",
+        "Whether the current temperature reading is valid.",
+        tags
+      ) { status(address)?.temperature?.isValid }
+      boolGauge(
+        "temperature.overheated",
+        "Whether the device is overheated.",
+        tags
+      ) { status(address)?.overTemperature }
 
-      boolGauge("update.available", tags) { status(address)?.update?.hasUpdate }
-      counter("uptime", "seconds", tags) { status(address)?.uptime }
-      gauge("filesystem.free", "bytes", tags) { status(address)?.fileSystemFree }
-      gauge("filesystem.size", "bytes", tags) { status(address)?.fileSystemSize }
-      gauge("memory.free", "bytes", tags) { status(address)?.ramFree }
-      gauge("memory.size", "bytes", tags) { status(address)?.ramTotal }
-      gauge("memory.low-water-mark", "bytes", tags) { status(address)?.ramLowWaterMark }
+      boolGauge(
+        "update.available",
+        "Whether a firmware update is available for the device.",
+        tags
+      ) { status(address)?.update?.hasUpdate }
+      counter(
+        "uptime",
+        "Device uptime in seconds.",
+        "seconds",
+        tags
+      ) { status(address)?.uptime }
+      gauge(
+        "filesystem.free",
+        "Filesystem free space in bytes.",
+        "bytes",
+        tags
+      ) { status(address)?.fileSystemFree }
+      gauge(
+        "filesystem.size",
+        "Filesystem total capacity in bytes.",
+        "bytes",
+        tags
+      ) { status(address)?.fileSystemSize }
+      gauge(
+        "memory.free",
+        "Free memory in bytes.",
+        "bytes",
+        tags
+      ) { status(address)?.ramFree }
+      gauge(
+        "memory.size",
+        "Total memory size in bytes.",
+        "bytes",
+        tags
+      ) { status(address)?.ramTotal }
+      gauge(
+        "memory.low-water-mark",
+        "Memory low water mark in bytes.",
+        "bytes",
+        tags
+      ) { status(address)?.ramLowWaterMark }
 
       val meterCount = catchingWithDefault(0) {
         shellyClient.status(address)?.meters?.size
@@ -83,12 +158,29 @@ class ShellyMetrics(
       for (index in 0 until meterCount) {
         val meterTags = tags.and(Tag.of(TAGNAME_CHANNEL, index.toString()))
 
-        counter("meter.power", "watthours", meterTags) {
-          (status(address)?.meters?.get(index)?.total ?: Double.NaN) / 60.0
-        }
-        gauge("meter.power.current", "watts", meterTags) { status(address)?.meters?.get(index)?.power }
-        gauge("meter.overpower", "watts", meterTags) { status(address)?.meters?.get(index)?.overpower }
-        boolGauge("meter.value.valid", meterTags) { status(address)?.meters?.get(index)?.isValid }
+        counter(
+          "meter.power",
+          "Total power consumption in watthours.",
+          "watthours",
+          meterTags
+        ) { (status(address)?.meters?.get(index)?.total ?: Double.NaN) / 60.0 }
+        gauge(
+          "meter.power.current",
+          "Momentary power consumption in watts.",
+          "watts",
+          meterTags
+        ) { status(address)?.meters?.get(index)?.power }
+        gauge(
+          "meter.overpower",
+          "Momentary power over-consumption in watts.",
+          "watts",
+          meterTags
+        ) { status(address)?.meters?.get(index)?.overpower }
+        boolGauge(
+          "meter.value.valid",
+          "Whether momentary readings are valid.",
+          meterTags
+        ) { status(address)?.meters?.get(index)?.isValid }
       }
 
       val outputCount = catchingWithDefault(0) {
@@ -97,18 +189,46 @@ class ShellyMetrics(
       for (index in 0 until outputCount) {
         val relayTags = tags.and(Tag.of(TAGNAME_CHANNEL, index.toString()))
 
-        boolGauge("relay.on", relayTags) { status(address)?.relays?.get(index)?.isOn }
-        boolGauge("relay.overpower", relayTags) { status(address)?.relays?.get(index)?.overpower }
-        boolGauge("relay.has-timer", relayTags) { status(address)?.relays?.get(index)?.hasTimer }
-        gauge("relay.timer-started", "seconds", relayTags) { status(address)?.relays?.get(index)?.timerStarted }
-        gauge("relay.timer-duration", "seconds", relayTags) { status(address)?.relays?.get(index)?.timerDuration }
-        gauge("relay.timer-remaining", "seconds", relayTags) { status(address)?.relays?.get(index)?.timerRemaining }
+        boolGauge(
+          "relay.on",
+          "Whether the relay is switched on.",
+          relayTags
+        ) { status(address)?.relays?.get(index)?.isOn }
+        boolGauge(
+          "relay.overpower",
+          "Whether the relay has detected overpower / overload.",
+          relayTags
+        ) { status(address)?.relays?.get(index)?.overpower }
+        boolGauge(
+          "relay.has-timer",
+          "Whether the relay has a timer set.",
+          relayTags
+        ) { status(address)?.relays?.get(index)?.hasTimer }
+        gauge(
+          "relay.timer-started",
+          "Seconds since the current relay timer has started, if any.",
+          "seconds",
+          relayTags
+        ) { status(address)?.relays?.get(index)?.timerStarted }
+        gauge(
+          "relay.timer-duration",
+          "Total duration of current relay timer, if any.",
+          "seconds",
+          relayTags
+        ) { status(address)?.relays?.get(index)?.timerDuration }
+        gauge(
+          "relay.timer-remaining",
+          "Remaining seconds of the current relay timer.",
+          "seconds",
+          relayTags
+        ) { status(address)?.relays?.get(index)?.timerRemaining }
       }
     }
   }
 
   private inline fun ShellyDevice.counter(
     name: String,
+    description: String,
     baseUnit: String?,
     tags: Tags,
     crossinline func: ShellyClient.() -> Number?
@@ -117,6 +237,7 @@ class ShellyMetrics(
       "Registered meter [{}]",
       FunctionCounter
         .builder(pre(name), this) { shellyClient.runCatching { func() }.getOrNull().orDefault() }
+        .description(description)
         .baseUnit(baseUnit)
         .tags(tags)
         .register(meterRegistry).id
@@ -125,6 +246,7 @@ class ShellyMetrics(
 
   private inline fun ShellyDevice.gauge(
     name: String,
+    description: String,
     baseUnit: String?,
     tags: Tags,
     crossinline func: ShellyClient.() -> Number?
@@ -133,6 +255,7 @@ class ShellyMetrics(
       "Registered meter [{}]",
       Gauge
         .builder(pre(name), this) { shellyClient.runCatching { func() }.getOrNull().orDefault() }
+        .description(description)
         .baseUnit(baseUnit)
         .tags(tags)
         .register(meterRegistry).id
@@ -143,10 +266,11 @@ class ShellyMetrics(
 
   private inline fun ShellyDevice.boolGauge(
     name: String,
+    description: String,
     tags: Tags,
     crossinline func: ShellyClient.() -> Boolean?
   ) {
-    gauge(name, null, tags) { func().toDouble() }
+    gauge(name, description, null, tags) { func().toDouble() }
   }
 
   private fun Number?.orDefault(): Double = this?.toDouble() ?: failureValue
