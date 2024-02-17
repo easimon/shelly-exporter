6da571d610c6168588579e0f485ed540a72a7705
Markus Dobel <github@spam.dobel.click>
2024-02-17 14:00:47 +0100

    fix: suppress spread operator warning

--- src/main/kotlin/click/dobel/shelly/exporter/ShellyExporterApplication.kt
+++ src/main/kotlin/click/dobel/shelly/exporter/ShellyExporterApplication.kt
@@ -7,5 +7,6 @@ import org.springframework.boot.runApplication
 class ShellyExporterApplication
 
 fun main(args: Array<String>) {
+  @Suppress("SpreadOperator")
   runApplication<ShellyExporterApplication>(*args)
 }
